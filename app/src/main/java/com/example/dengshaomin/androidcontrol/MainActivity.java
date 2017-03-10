44209468cc28929c78796adcd13ac8afa8767310
nothing <nocoding@163.com>
2017-03-10 14:34:30 +0800

    button,代码动态设置样式，拒绝xml

--- app/src/main/java/com/example/dengshaomin/androidcontrol/MainActivity.java
+++ app/src/main/java/com/example/dengshaomin/androidcontrol/MainActivity.java
@@ -1,38 +1,14 @@
 package com.example.dengshaomin.androidcontrol;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.Toast;
-
-import com.ToxicBakery.viewpager.transforms.CubeInTransformer;
-import com.bigkoo.convenientbanner.ConvenientBanner;
-import com.bigkoo.convenientbanner.holder.CBViewHolderCreator;
-import com.bigkoo.convenientbanner.holder.Holder;
-import com.bigkoo.convenientbanner.listener.OnItemClickListener;
-import com.bumptech.glide.Glide;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SuppressLint("NewApi")
 public class MainActivity extends AppCompatActivity {
 
-    private ConvenientBanner convenientBanner;
-    private List<String> localImages = new ArrayList<String>() {{
-        add("http://static.g.iqiyi.com/operation/img/201612/5866441a6256e.png");
-        add("http://static.g.iqiyi.com/operation/img/201612/58662abc4f54f.png");
-        add("http://static.g.iqiyi.com/operation/img/201612/58662cc7cff72.jpg");
-        add("http://static.g.iqiyi.com/operation/img/201701/586afe50c9aba.png");
-
-    }};
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
@@ -45,64 +21,7 @@ public class MainActivity extends AppCompatActivity {
     //初始化控件
     @RequiresApi(api = Build.VERSION_CODES.M)
     private void initView() {
-        convenientBanner = (ConvenientBanner) findViewById(R.id.convenientBanner);
-        //自定义你的Holder，实现更多复杂的界面，不一定是图片翻页，其他任何控件翻页亦可。
-        convenientBanner.setPages(
-                new CBViewHolderCreator<LocalImageHolderView>() {
-                    @Override
-                    public LocalImageHolderView createHolder() {
-                        return new LocalImageHolderView();
-                    }
-                }, localImages)
-                //设置两个点图片作为翻页指示器，不设置则没有指示器，可以根据自己需求自行配合自己的指示器,不需要圆点指示器可用不设
-                .setPageIndicator(new int[]{R.drawable.dot_green, R.drawable.dot_yellow})
-                //设置指示器的方向
-                .setPageTransformer(new CubeInTransformer())
-                .setPageIndicatorAlign(ConvenientBanner.PageIndicatorAlign.ALIGN_PARENT_RIGHT);
-        //设置翻页的效果，不需要翻页效果可用不设
-//        convenientBanner.setManualPageable(false);//设置不能手动影响
-        convenientBanner.startTurning(3000);
-        convenientBanner.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                Toast.makeText(MainActivity.this,position + "",Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        });
-        convenientBanner.setOnItemClickListener(new OnItemClickListener() {
-            @Override
-            public void onItemClick(int position) {
-                Toast.makeText(MainActivity.this,position + "",Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
-    public class LocalImageHolderView implements Holder<String> {
-        private ImageView imageView;
-
-        @Override
-        public View createView(Context context) {
-            imageView = new ImageView(context);
-            imageView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
-            return imageView;
-        }
-
-        @Override
-        public void UpdateUI(Context context, final int position, String data) {
-            Glide.with(context)
-                    .load(data)
-                    .into(imageView);
-        }
     }
 
 }
